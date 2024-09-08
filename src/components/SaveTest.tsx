cc8f2258e62be0f68ceba1913f62381f0345c765
umekan <unigori226@gmail.com>
2024-09-09 05:30:18 +0900

    冗長

--- src/components/SaveTest.tsx
+++ src/components/SaveTest.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
+import { useState, useEffect } from "react";
 
 export default function SaveTest() {
-    const [value, setValue] = React.useState(() => localStorage.getItem('value') ? JSON.parse(localStorage.getItem('value')!) : 0);
+    const [value, setValue] = useState(() => localStorage.getItem('value') ? JSON.parse(localStorage.getItem('value')!) : 0);
 
     // 値が変更されたらローカルストレージに保存
-    React.useEffect(() => {
+    useEffect(() => {
         localStorage.setItem('value', JSON.stringify(value));
     }, [value]);
 
     return (
         <div>
             <p>
-                <button onClick={() => setValue(value + 1)}>Increment</button>
                 <button onClick={() => setValue(value - 1)}>Decrement</button>
+                <button onClick={() => setValue(value + 1)}>Increment</button>
             </p>
             <p>{value}</p>
         </div>
